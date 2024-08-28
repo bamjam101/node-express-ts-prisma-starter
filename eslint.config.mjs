ce1a85d559e3a02149c0df44fcd36774920ad201
bamjam101 <herejamil123@gmail.com>
2024-08-28 13:02:44 +0530

    setup: Eslint and prettier for Typescript based backend

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import eslint from "@eslint/js"
+import eslintConfigPrettier from "eslint-config-prettier"
+import tseslint from "typescript-eslint"
+
+export default tseslint.config({
+	languageOptions: {
+		parserOptions: {
+			project: true,
+			// @ts-ignore
+			tsconfigRootDir: import.meta.dirname,
+		},
+	},
+	files: ["**/*.ts"],
+	extends: [
+		eslint.configs.recommended,
+		...tseslint.configs.recommendedTypeChecked,
+		eslintConfigPrettier,
+	],
+	rules: {
+		"no-useless-catch": "error",
+		quotes: ["error", "single", { allowTemplateLiterals: true }],
+	},
+})
